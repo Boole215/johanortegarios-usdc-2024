6668804c8fdf30ce013dd5efe02e91753770028d
Johan Ortega <hal215@outlook.com>
2023-12-03 21:03:42 -0600

    Task mostly complete, need to cover a remaining edge-case

---
+++ book_search.js
@@ -0,0 +1,338 @@
+/**
+ * RECOMMENDATION
+ *
+ * To test your code, you should open "tester.html" in a web browser.
+ * You can then use the "Developer Tools" to see the JavaScript console.
+ * There, you will see the results unit test execution. You are welcome
+ * to run the code any way you like, but this is similar to how we will
+ * run your code submission.
+ *
+ * The Developer Tools in Chrome are available under the "..." menu,
+ * futher hidden under the option "More Tools." In Firefox, they are
+ * under the hamburger (three horizontal lines), also hidden under "More Tools."
+ */
+
+/**
+ * Replaces symbols in text with spaces so it is easily splittable.
+ * @param {string} text - The text to be sanitized.
+ * @returns {string} - Sanitized text.
+ * */
+function stripSymbolsFromText(text){
+    // First, we replace characters that aren't alphanumeric, hyphens, apostrophes, with spaces.
+    //    We keep hyphens because some words must be hyphenated to be correct.
+    // Then, we reduce groups of multiple spaces to single spaces.
+    let sanitized = text.replace(/[^a-zA-Z0-9-']/g, ' ').replace(/\s+/g, ' ');
+    return sanitized;
+}
+
+/**
+ * Searches for matches in scanned text.
+ * @param {string} searchTerm - The word or term we're searching for.
+ * @param {JSON} scannedTextObj - A JSON object representing the scanned text.
+ * @returns {JSON} - Search results.
+ * */
+function findSearchTermInBooks(searchTerm, scannedTextObj) {
+    /** You will need to implement your search and
+     * return the appropriate object here. */
+    // Index of words and their locations
+    let word_index = {};
+
+    for (const scannedText of scannedTextObj){
+	const textISBN = scannedText.ISBN;
+	const textContent = scannedText.Content;
+
+	// Iterate over each entry in 'Content'
+	for (const content of textContent) {
+	    // Get the indivual words from the (sanitized) line's text
+	    const plaintext = stripSymbolsFromText(content.Text).split(" ");
+	    // Iterate over each word in the line
+	    for (const word of plaintext) {
+		const wordLocation = {
+		    "ISBN": textISBN,
+		    "Page": content.Page,
+		    "Line": content.Line
+		}
+
+		if (! (word in word_index)) {
+		    word_index[word] = [wordLocation];
+		}
+		else {
+		    word_index[word].push(wordLocation);
+		}
+	    }
+	}
+    }
+
+    const searchResult = searchTerm in word_index ? word_index[searchTerm] : [];
+    var result = {
+        "SearchTerm": searchTerm,
+        "Results": searchResult
+    };
+
+    return result;
+}
+
+/** Sample Text Objects */
+const twentyLeagues = {
+        "Title": "Twenty Thousand Leagues Under the Sea",
+        "ISBN": "9780000528531",
+        "Content": [
+            {
+                "Page": 31,
+                "Line": 8,
+                "Text": "now simply went on by her own momentum.  The dark-"
+            },
+            {
+                "Page": 31,
+                "Line": 9,
+                "Text": "ness was then profound; and however good the Canadian\'s"
+            },
+            {
+                "Page": 31,
+                "Line": 10,
+                "Text": "eyes were, I asked myself how he had managed to see, and"
+            }
+        ]
+};
+
+const mythOfSisyphus = {
+    "Title": "The Myth of Sisyphus",
+    "ISBN": "9780525564454",
+    "Content": [
+	{
+	    "Page": 14,
+	    "Line": 1,
+	    "Text": "he recognizes his worst enemy. Tomorrow, he was long-"
+	},
+	{
+	    "Page": 14,
+	    "Line": 2,
+	    "Text": "ing for tomorrow, whereas everything in him ought to"
+	},
+	{
+	    "Page": 14,
+	    "Line": 3,
+	    "Text": "reject it. That revolt of the flesh is the absurd."
+	},
+	{
+	    "Page": 16,
+	    "Line": 11,
+	    "Text": "I am interested—let me re-"
+	},
+	{
+	    "Page": 16,
+	    "Line": 12,
+	    "Text": "peat again—not so much in absurd discoveries as in their"
+	},
+	{
+	    "Page": 16,
+	    "Line": 13,
+	    "Text": "consequences. If one is assured of these facts, what is"
+	},
+	{
+	    "Page": 16,
+	    "Line": 27,
+	    "Text": "the falsity of our own thesis (for the contrary assertion"
+	},
+	{
+	    "Page": 16,
+	    "Line": 28,
+	    "Text": "does not admit that it can be true). And if one says that"
+	}
+    ]
+};
+
+/** Example input object. */
+const twentyLeaguesIn = [ twentyLeagues ]
+const mythOfSisyphusIn = [ mythOfSisyphus ]
+const mythOfLeaguesIn = [ twentyLeagues, mythOfSisyphus ]
+
+/** Example output object */
+const twentyLeaguesOut = {
+    "SearchTerm": "the",
+    "Results": [
+        {
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 9
+        }
+    ]
+}
+
+/*
+ _   _ _   _ ___ _____   _____ _____ ____ _____ ____
+| | | | \ | |_ _|_   _| |_   _| ____/ ___|_   _/ ___|
+| | | |  \| || |  | |     | | |  _| \___ \ | | \___ \
+| |_| | |\  || |  | |     | | | |___ ___) || |  ___) |
+ \___/|_| \_|___| |_|     |_| |_____|____/ |_| |____/
+
+ */
+
+console.log("Beginning stripSymbolsFromText Testing");
+/*
+ * TEST String Sanitizer
+ */
+
+
+// TEST 1 - Stripping parentheses
+const testSanitize1result = stripSymbolsFromText("the falsity of our own thesis (for the contrary assertion");
+const testSanitize1expected = "the falsity of our own thesis for the contrary assertion";
+if (testSanitize1result == testSanitize1expected){
+    console.log("PASS: Test 1");
+} else{
+    console.log("FAIL: Test 1");
+    console.log("Expected:", testSanitize1result);
+    console.log("Received:", testSanitize1expected);
+}
+
+// TEST 2 - Stripping Em-dashes
+const testSanitize2result = stripSymbolsFromText("I am interested—let me re-");
+const testSanitize2expected = "I am interested let me re-";
+if (testSanitize2result == testSanitize2expected){
+    console.log("PASS: Test 2");
+} else{
+    console.log("FAIL: Test 2");
+    console.log("Expected:", testSanitize2result);
+    console.log("Received:", testSanitize2expected);
+}
+
+/* We have provided two unit tests. They're really just `if` statements that
+ * output to the console. We've provided two tests as examples, and
+ * they should pass with a correct implementation of `findSearchTermInBooks`.
+ *
+ * Please add your unit tests below.
+ * */
+console.log("Beginning findSearchTermInBooks Testing");
+// TEST 1
+/** We can check that, given a known input, we get a known output. */
+const test1result = findSearchTermInBooks("the", twentyLeaguesIn);
+if (JSON.stringify(twentyLeaguesOut) === JSON.stringify(test1result)) {
+    console.log("PASS: Test 1");
+} else {
+    console.log("FAIL: Test 1");
+    console.log("Expected:", twentyLeaguesOut);
+    console.log("Received:", test1result);
+}
+
+// TEST 2
+/** We could choose to check that we get the right number of results. */
+const test2result = findSearchTermInBooks("the", twentyLeaguesIn);
+if (test2result.Results.length == 1) {
+    console.log("PASS: Test 2");
+} else {
+    console.log("FAIL: Test 2");
+    console.log("Expected:", twentyLeaguesOut.Results.length);
+    console.log("Received:", test2result.Results.length);
+}
+
+// TEST 3 - Check search(the) != search(The)
+const test3resultA = findSearchTermInBooks("the", twentyLeaguesIn);
+const test3resultB = findSearchTermInBooks("The", twentyLeaguesIn);
+if (JSON.stringify(test3resultA) == JSON.stringify(test3resultB)){
+    console.log("FAIL: Test 3");
+    console.log("'the' Search Result:", atheResult)
+    console.log("'The' Search Result", aTheResult)
+} else {
+    console.log("PASS: Test 3")
+}
+
+// TEST 4 - Check for punctuated search "Canadian's"
+const test4result = findSearchTermInBooks("Canadian\'s", twentyLeaguesIn);
+const test4expected = {
+    "SearchTerm": "Canadian\'s",
+    "Results": [
+	{
+	    "ISBN": "9780000528531",
+	    "Page": 31,
+	    "Line": 9
+	}]};
+if (JSON.stringify(test4result) === JSON.stringify(test4expected)) {
+    console.log("PASS: Test 4");
+} else{
+    console.log("FAIL: Test 4");
+    console.log("Expected:", test4expected);
+    console.log("Received:", test4result);
+}
+
+// Test 5 - Test with text containing symbols
+// Testing with periods 'absurd.'
+const test5_1result = findSearchTermInBooks("absurd", mythOfSisyphusIn);
+const test5_1expected = {
+    "SearchTerm": "absurd",
+    "Results": [
+	{
+	    "ISBN": "9780525564454",
+	    "Page": 14,
+	    "Line": 3,
+	},
+	{
+	    "ISBN": "9780525564454",
+	    "Page": 16,
+	    "Line": 12,
+	}]
+};
+if (JSON.stringify(test5_1result) === JSON.stringify(test5_1expected)) {
+    console.log("PASS: Test 5_1");
+} else{
+    console.log("FAIL: Test 5_1");
+    console.log("Expected:", test5_1expected);
+    console.log("Received:", test5_1result);
+}
+// Testing with em-dash 'interested—'
+const test5_2result = findSearchTermInBooks("interested", mythOfSisyphusIn);
+const test5_2expected = {
+    "SearchTerm": "interested",
+    "Results": [{
+	"ISBN": "9780525564454",
+	"Page": 16,
+	"Line": 11
+    }]
+}
+if (JSON.stringify(test5_2result) === JSON.stringify(test5_2expected)) {
+    console.log("PASS: Test 5_2");
+} else{
+    console.log("FAIL: Test 5_2");
+    console.log("Expected:", test5_2expected);
+    console.log("Received:", test5_2result);
+}
+
+// Test 6 - Test with multiple texts
+const test6result = findSearchTermInBooks("the", mythOfLeaguesIn);
+const test6expected = {
+    "SearchTerm": "the",
+    "Results": [
+	{
+            "ISBN": "9780000528531",
+            "Page": 31,
+            "Line": 9
+	},
+	{
+	    "ISBN": "9780525564454",
+	    "Page": 14,
+	    "Line": 3
+	},
+	{
+	    "ISBN": "9780525564454",
+	    "Page": 14,
+	    "Line": 3
+	},
+	{
+	    "ISBN": "9780525564454",
+	    "Page": 16,
+	    "Line": 27
+	},
+	{
+	    "ISBN": "9780525564454",
+	    "Page": 16,
+	    "Line": 27
+	},
+
+    ]
+};
+if (JSON.stringify(test6result) === JSON.stringify(test6expected)){
+    console.log("PASS: Test 6");
+} else {
+    console.log("FAIL: Test 6");
+    console.log("Expected:", test6expected);
+    console.log("Received:", test6result);
+}
